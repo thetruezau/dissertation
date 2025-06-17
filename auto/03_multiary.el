231b424f2f6f656d39efa10c5714cf71cf6fe9fe
Miguel Alves <miglas.toiglas@gmail.com>
2025-06-17 17:27:27 +0100

    small steps

---
+++ auto/03_multiary.el
@@ -0,0 +1,12 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "03_multiary"
+ (lambda ()
+   (LaTeX-add-labels
+    "c:multiary"
+    "eq:ruleb1"
+    "eq:ruleb2"
+    "eq:ruleh"))
+ :latex)
+
